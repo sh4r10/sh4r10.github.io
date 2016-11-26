34107d6cc5afc9188d0c9b4bf7f2b032cfab9b17
Shariq Shahbaz <shariq.shahbaz.12@gmail.com>
2016-11-26 13:53:42 +0000

    added all the files and the button

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = ready;
+
+function ready(){
+    
+  document.getElementById("hilo").addEventListener("click", hilo)
+    
+    
+    
+    
+    
+    
+    
+    function hilo(){
+        window.location = "hilo/hilo.html";
+    }
+    
+    
+}
