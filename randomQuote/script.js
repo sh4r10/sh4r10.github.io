3e54fb4e00e4a23d11c44624f60c3908d7a69877
Shariq Shahbaz <shariq.shahbaz.12@gmail.com>
2016-11-26 20:41:04 +0000

    fixed a minor bug

--- randomQuote/script.js
+++ randomQuote/script.js
@@ -1,7 +1,7 @@
 window.onload = function ready(){
   
   document.getElementById("refresh").addEventListener("click", ready);
-  
+  document.getElementById("dnl").addEventListener("click", download);
   var random = Math.floor((Math.random() * 5) + 1);
   console.log(random);
   
