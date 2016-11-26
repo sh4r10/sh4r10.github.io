174084618c642c5d3ff31402c924bcda76f9e0ca
Shariq Shahbaz <shariq.shahbaz.12@gmail.com>
2016-11-26 20:38:21 +0000

    added the random quote machine

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ window.onload = ready;
 function ready(){
     
   document.getElementById("hilo").addEventListener("click", hilo)
+  document.getElementById("randomQuote").addEventListener("click", randomQuote)
     
     
     
     
     
-    
+    function randomQuote(){
+      window.location = "randomQuote.html";
+    }
     
     function hilo(){
         window.location = "hilo.html";
