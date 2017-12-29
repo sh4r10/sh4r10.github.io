dde899cffff9af7cd42ce62b7a06cd84d6a3c714
sh4r10 <sh4r10.c0d3@gmail.com>
2017-12-29 16:51:28 +0100

    created assets folder

---
+++ src/js/spinMe.js
@@ -0,0 +1,36 @@
+var repeat;
+var element;
+var aud;
+var degrees = 0;
+var count = 1;
+
+
+
+function setAudio(source){
+ aud = new Audio(source);
+}
+
+function rotate(comp, speed) {
+  element = document.getElementById(comp);
+  element.style.transform = "rotate(" + degrees + "deg)";
+
+  repeat = setTimeout("rotate('" + comp + "'," + speed + ")", speed);
+  degrees++;
+  if (degrees > 359) {
+    degrees = 1;
+  }
+}
+
+document.getElementById("spinMe").addEventListener("click", function() {
+  if (count == 0) {
+    clearTimeout(repeat);
+    count = 1;
+    aud.pause();
+  } else if (count == 1) {
+    count = 0;
+    rotate("spinMe", 8);
+    aud.play();
+  }
+});
+
+setAudio("https://raw.githubusercontent.com/sh4r10/sh4r10.github.io/master/song.mp3");
